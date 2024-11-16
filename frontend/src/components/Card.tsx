f82d435c205fd97d167818ed5b05c2bb65dcb1cb
FenrisWolf <albertomg@hotmail.com>
2024-11-16 12:14:11 -0600

    Se añade un mejor svg

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -11,10 +11,10 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({key, imageGame, imageText, game, earn }) => {
   return (
     <div className="cursor-pointer flex flex-row md:flex-col gap-4 rounded-lg shadow-lg">
-      <div className="w-1/4 md:w-full md:h-full md:flex-1">
+      <div className="w-1/4 md:w-full md:flex-1">
         <img
           src={imageGame}
-          className="object-cover rounded-l-lg md:rounded-bl-none md:rounded-t-lg"
+          className="h-full object-cover rounded-l-lg md:rounded-bl-none md:rounded-t-lg"
           alt={imageText}
         />
       </div>
