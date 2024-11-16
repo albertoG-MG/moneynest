0983630c6c8e7d84753240affece8d91eac5c291
FenrisWolf <albertomg@hotmail.com>
2024-11-16 11:56:15 -0600

    Se arregla el texto cuando es mucho

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -11,15 +11,15 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({key, imageGame, imageText, game, earn }) => {
   return (
     <div className="cursor-pointer flex flex-row md:flex-col gap-4 rounded-lg shadow-lg">
-      <div className="w-1/4 md:w-full md:h-full  md:flex-1">
+      <div className="w-1/4 md:w-full md:h-full md:flex-1">
         <img
           src={imageGame}
           className="object-cover rounded-l-lg md:rounded-bl-none md:rounded-t-lg"
           alt={imageText}
         />
       </div>
-      <div className="flex-1 md:flex-none font-semibold py-4 md:p-4 leading-8">
-        <div className="flex flex-col-reverse md:flex-col">
+      <div className="flex-1 md:flex-none font-semibold py-4 pr-2 md:p-4 leading-8">
+        <div className="flex flex-col-reverse md:flex-col" style={{ wordBreak: 'break-word' }}>
           <p className="block text-md md:text-lg">{game}</p>
           <p className="block text-md">
             <span className="text-green-500">{earn}</span>
