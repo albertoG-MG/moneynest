70bfb598ea946b4ad019d75ba8a95fa3445dd774
FenrisWolf <albertomg@hotmail.com>
2024-11-21 09:47:34 -0600

    Se agrega la parte de los compañeros

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -39,6 +39,51 @@ const data = [
   },
 ];
 
+const partners = [
+  {
+    id: 1,
+    imagePartners: 'https://s3-eu-west-1.amazonaws.com/tpd/logos/61734f690e82255e1b6fa7fd/0x0.png',
+    imagePartnersText: 'Revu',
+    partner: 'Revu',
+    earn_partner: 'Gana hasta 3,712 HC',
+  },
+  {
+    id: 2,
+    imagePartners: 'https://styles.redditmedia.com/t5_7kpqm7/styles/communityIcon_vs1g8hxifn5a1.png',
+    imagePartnersText: 'MyChips',
+    partner: 'MyChips',
+    earn_partner: 'Gana hasta 6,120 HC',
+  },
+  {
+    id: 3,
+    imagePartners: 'https://media.licdn.com/dms/image/v2/D4D0BAQFtXXYQdOW1RQ/company-logo_200_200/company-logo_200_200/0/1701182702076/offertoro_logo?e=2147483647&v=beta&t=PB9Hwv4RVNluUx-1TTJBd7yq1xIexPmV6MRLxIqOppQ',
+    imagePartnersText: 'Torox',
+    partner: 'Torox',
+    earn_partner: 'Gana hasta 5,000 HC',
+  },
+  {
+    id: 4,
+    imagePartners: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTBRtvN0gXT6PAetK3yoixzqvzmSKmwb5qzg&s',
+    imagePartnersText: 'Lootably',
+    partner: 'Lootably',
+    earn_partner: 'Gana hasta 4,800 HC',
+  },
+  {
+    id: 5,
+    imagePartners: 'https://media.licdn.com/dms/image/v2/C560BAQEdvv1GfEzC1g/company-logo_200_200/company-logo_200_200/0/1631336722592?e=2147483647&v=beta&t=HzLNTxTRqqTj8mq72TqorkSXyx-WTEZAKnkdzu14zSQ',
+    imagePartnersText: 'AdscendMedia',
+    partner: 'AdscendMedia',
+    earn_partner: 'Gana hasta 1,713 HC',
+  },
+  {
+    id: 6,
+    imagePartners: 'https://media.licdn.com/dms/image/v2/C4D0BAQFkekiBlRyODg/company-logo_200_200/company-logo_200_200/0/1631319440419?e=2147483647&v=beta&t=j76ycGTM6bcP2dQUO9OchFiOjdtnu1682Z3M-e-B1S4',
+    imagePartnersText: 'Wannads',
+    partner: 'Wannads',
+    earn_partner: 'Gana hasta 611 HC',
+  },
+];
+
 const Home: React.FC = () => {
     return (
         <>
@@ -160,6 +205,25 @@ const Home: React.FC = () => {
                 imageText={item.imageText}
                 game={item.game}
                 earn={item.earn}
+              />
+            ))}           
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="flex items-center justify-between gap-3 px-4">
+              <div className="flex-1 flex items-center gap-3 pr-2.5">
+                <svg fill="#000000" className="w-10 h-10" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 293.953 293.953" xmlSpace="preserve"><g><path d="M219.602,99.555h-65.125v-19.43c0-18.985-15.445-34.431-34.43-34.431H85.424c-4.143,0-7.5,3.358-7.5,7.5s3.357,7.5,7.5,7.5h34.623c10.714,0,19.43,8.717,19.43,19.431v19.43H74.352C33.354,99.555,0,132.909,0,173.906c0,40.998,33.354,74.353,74.352,74.353c29.215,0,55.804-17.355,67.75-43.692h9.751c11.945,26.338,38.533,43.692,67.749,43.692c40.998,0,74.352-33.355,74.352-74.353C293.953,132.909,260.6,99.555,219.602,99.555z M219.602,233.259c-24.741,0-47.122-15.589-55.691-38.791c-1.088-2.946-3.896-4.902-7.035-4.902H137.08c-3.14,0-5.947,1.956-7.035,4.901c-8.571,23.202-30.952,38.791-55.693,38.791C41.625,233.259,15,206.633,15,173.906c0-32.727,26.625-59.352,59.352-59.352h145.25c32.727,0,59.352,26.625,59.352,59.352C278.953,206.633,252.328,233.259,219.602,233.259z"/><path d="M101.957,166.406H81.852V146.07c0-4.142-3.357-7.5-7.5-7.5s-7.5,3.358-7.5,7.5v20.336H46.746c-4.143,0-7.5,3.358-7.5,7.5c0,4.142,3.357,7.5,7.5,7.5h20.105v20.337c0,4.142,3.357,7.5,7.5,7.5s7.5-3.358,7.5-7.5v-20.337h20.105c4.143,0,7.5-3.358,7.5-7.5C109.457,169.764,106.1,166.406,101.957,166.406z"/><circle cx="219.602" cy="148.203" r="10.668"/><circle cx="219.602" cy="199.608" r="10.668"/><circle cx="245.305" cy="173.907" r="10.668"/><circle cx="193.898" cy="173.907" r="10.668"/></g></svg>
+                <h1 className="font-inter text-2x1 md:text-3xl font-bold text-black">Nuestros compañeros!</h1>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row gap-3 py-4 overflow-x-auto overflow-y-hidden px-4">
+            {partners.map((item, index) => (
+              <Card
+                key={item.id}
+                imageGame={item.imagePartners}
+                imageText={item.imagePartnersText}
+                game={item.partner}
+                earn={item.earn_partner}
               />
             ))}           
             </div>
